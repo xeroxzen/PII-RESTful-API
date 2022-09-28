0fac7591ec5dc6647886801a64d1f452cec3d6e4
Google Jr <andilembele020@gmail.com>
2022-09-28 09:43:10 -0500

    'AutoSchema' from restframework

--- piiAPI/settings.py
+++ piiAPI/settings.py
@@ -54,6 +54,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
 
 MIDDLEWARE = [
